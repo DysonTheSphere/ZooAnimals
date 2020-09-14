3aec0f23bc2e9be31d09744e89511b1bc805c9de
Daniel Winston <disabledfishinabarrel@gmail.com>
2020-09-13 20:22:19 -0600

    Initial commit

---
+++ Animal.java
@@ -0,0 +1,17 @@
+package ZooAnimals;
+
+public class Animal 
+{
+	private String name;
+	
+	public Animal(String name)
+	{
+		this.name = name;
+	}
+	
+	String wakeUp;
+	String makeNoise;
+	String roam;
+	String sleep;	
+	String eat;
+}
---
+++ Wolf.java
@@ -0,0 +1,6 @@
+package ZooAnimals;
+
+public class Wolf 
+{
+
+}
