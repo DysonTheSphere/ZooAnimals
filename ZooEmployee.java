5c98083203a672a514cbd87d6ed57ba33d6a02f7
icecreamlollipop <nicole.dong@gmail.com>
2020-09-16 01:18:51 -0600

    yeeeeeeeeeeeeeeeeet

--- ZooEmployee.java
+++ ZooEmployee.java
@@ -46,27 +46,8 @@ public class ZooEmployee extends ZooKeeper
 	{
 		for (ZooAnimals.Animal a : zoo)
 		{
-			int rng = (int) ((Math.random() * 100) + 1);
-			
-			//Special Cases
-			//dog digs 25% of the time
-			if (a.getClass().getSimpleName() == "Dog")
-			{
-				ZooAnimals.Dog dog = (Dog) a;
-				if (rng <= 25)
-					dog.dig();
-				else
-					dog.roam();
-			}else if (a.getClass().getSuperclass().getSimpleName() == "Pachyderm") //Pachyderm charges 25% of the time
-			{
-				Pachyderm pachy = (Pachyderm) a;
-				if (rng <= 25)
-					pachy.charge();
-				else
-					pachy.roam();
-			}else a.roam();
-			
-			
+			System.out.println("Zookeeper tells " + a.getName() + " to exercise.");
+			a.roam();
 		}
 	}
 
@@ -77,18 +58,8 @@ public class ZooEmployee extends ZooKeeper
 		for (ZooAnimals.Animal a : zoo)
 		{
 			System.out.println("Zookeeper tucks in " + a.getName());
-			//Cats don't care what you tell them
-			if (a.getClass().getSuperclass().getSimpleName() == "Feline")
-			{
-				int rng = (int) ((Math.random() * 100) + 1);
-				if (rng <= 30)
-					a.roam();
-				if (rng > 70)
-					a.makeNoise();
-				else
-					a.sleep();
-					
-			}
+			
+			a.sleep();
 		}
 	}
 
